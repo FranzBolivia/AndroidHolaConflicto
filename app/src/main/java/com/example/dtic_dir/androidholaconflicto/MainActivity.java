7d5cad5fbc38cd8bb6a82bc4209a3f64c3aafbe1
Franz Villalpando <villalpando.franz@usfx.bo>
2017-10-03 00:30:35 -0400

    Cambio realizao por el programador 2 otrito

--- app/src/main/java/com/example/dtic_dir/androidholaconflicto/MainActivity.java
+++ app/src/main/java/com/example/dtic_dir/androidholaconflicto/MainActivity.java
@@ -11,4 +11,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     //Comntario añadido por el programdaor dos
+    //adiciono otrito
 }
